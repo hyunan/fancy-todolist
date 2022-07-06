732406a6dcea4bc87da2d97c954b4a8f7464a9e4
ibepurplee <hyunseok930@gmail.com>
2022-07-06 08:32:11 -0700

    notes page addition

---
+++ src/screens/notes-screen.tsx
@@ -0,0 +1,16 @@
+import { Box, Text, ScrollView, useColorModeValue } from 'native-base'
+import Navbtn from '../components/navbtn'
+import MastHead from '../components/mast'
+
+export default function NotesScreen() {
+  return (
+    <Box bg={useColorModeValue('white', 'gray.900')} flex={1}>
+      <MastHead title='Settings'>
+        <Navbtn />
+      </MastHead>
+      <ScrollView>
+        <Text>Hello</Text>
+      </ScrollView>
+    </Box>
+  )
+}
