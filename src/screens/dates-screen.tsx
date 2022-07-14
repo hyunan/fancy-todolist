9141ce0207d21f373d02f2f09f8cef455611eca5
ibepurplee <hyunseok930@gmail.com>
2022-07-14 08:05:41 -0700

    stuff

---
+++ src/screens/dates-screen.tsx
@@ -0,0 +1,16 @@
+import { Box, Text, ScrollView, useColorModeValue } from 'native-base'
+import Navbtn from '../components/navbtn'
+import MastHead from '../components/mast'
+
+export default function DatesScreen() {
+  return (
+    <Box bg={useColorModeValue('white', 'gray.900')} flex={1}>
+      <MastHead title='Dates'>
+        <Navbtn />
+      </MastHead>
+      <ScrollView>
+        <Text>Hello</Text>
+      </ScrollView>
+    </Box>
+  )
+}
